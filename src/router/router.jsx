04d2113be63dfca62d2992c6a5469bceed370f13
Ramon <ramonmartinezcordero@gmail.com>
2024-04-16 23:04:34 -0400

    init route

---
+++ src/router/router.jsx
@@ -0,0 +1,32 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import { Root } from "../Root";
+import { AuthLayout, DashboardLayout } from "../layouts";
+import { LoginPage } from "../pages";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      /// Dashboard Routes
+      {
+        path: "dashboard",
+        element: <DashboardLayout />,
+        children: [],
+      },
+
+      /// Auth Routes
+      {
+        path: "auth",
+        element: <AuthLayout />,
+        children: [
+          {
+            path: "login",
+            element: <LoginPage />,
+          },
+        ],
+      },
+    ],
+  },
+]);
