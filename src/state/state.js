e75ebf8f2fd14a4e96735b7d0ebc19b6cfc4391f
Ramon <ramonmartinezcordero@gmail.com>
2024-04-17 23:18:29 -0400

    separacion de componentes

--- src/state/state.js
+++ src/state/state.js
@@ -3,4 +3,8 @@ import { create } from 'zustand'
 export const storeBulkBuddies = create((set) => ({
   mobileMenuOpen: false,
   setMobileMenuOpen: (value) => set(() => ({ mobileMenuOpen: value })),
+  userState: {},
+  setUserState: (value) => set(() => ({ userState: value })),
+  isAuth: false,
+  setIsAuth: (value) => set(() => ({ isAuth: value })),
 }))
